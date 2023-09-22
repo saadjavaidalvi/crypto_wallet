4bb6e8e72312c0167bd8dac6ab6f3a2bb54bbd9d
Saad Javaid Alvi <saadjavaidalvi@gmail.com>
2023-09-22 22:57:13 +0500

    updated redux for coinList

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
 import 'package:cryp_wallet/redux/app_state.dart';
+import 'package:cryp_wallet/redux/reducer.dart';
 import 'package:cryp_wallet/screens/splash/splash_screen.dart';
 import 'package:cryp_wallet/helpers/my_shared_preferences.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:redux/redux.dart';
+import 'package:redux_thunk/redux_thunk.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await MySharedPreference().init();
-  runApp(const MyApp());
+
+  final Store<AppState> store = Store<AppState>(
+    appReducer,
+    initialState: AppState.initial(),
+    middleware: [thunkMiddleware],
+  );
+  runApp(MyApp(
+    store: store,
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -27,32 +38,30 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     // return
     // Sizer(builder: (context, orientation, deviceType) {
-    return
-        /* StoreProvider<AppState>(
+    return StoreProvider<AppState>(
       store: widget.store!,
-      child:  */
-        GetMaterialApp(
-      title: 'Tavecchiacoin',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: const MaterialColor(
-          0xff4BA8E6,
-          <int, Color>{
-            50: Color(0xff4BA8E6),
-            100: Color(0xff4BA8E6),
-            200: Color(0xff4BA8E6),
-            300: Color(0xff4BA8E6),
-            400: Color(0xff4BA8E6),
-            500: Color(0xff4BA8E6),
-            600: Color(0xff4BA8E6),
-            700: Color(0xff4BA8E6),
-            800: Color(0xff4BA8E6),
-            900: Color(0xff4BA8E6),
-          },
+      child: GetMaterialApp(
+        title: 'Tavecchiacoin',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: const MaterialColor(
+            0xff4BA8E6,
+            <int, Color>{
+              50: Color(0xff4BA8E6),
+              100: Color(0xff4BA8E6),
+              200: Color(0xff4BA8E6),
+              300: Color(0xff4BA8E6),
+              400: Color(0xff4BA8E6),
+              500: Color(0xff4BA8E6),
+              600: Color(0xff4BA8E6),
+              700: Color(0xff4BA8E6),
+              800: Color(0xff4BA8E6),
+              900: Color(0xff4BA8E6),
+            },
+          ),
         ),
+        home: const SplashScreen(),
       ),
-      home: const SplashScreen(),
-      // ),
     );
     // });
   }
--- lib/redux/reducer.dart
+++ lib/redux/reducer.dart
@@ -1,104 +1,17 @@
-// import 'package:currency/redux/actions.dart';
-// import 'package:currency/utils/my_shared_preferences.dart';
+import 'package:cryp_wallet/redux/actions.dart';
 
-// import 'app_state.dart';
+import 'app_state.dart';
 
-// AppState appReducer(AppState state, action) {
-//   if (action is UpdateProfile) {
-//     return _updateProfile(state, action);
-//   } else if (action is UpdateUsdRate) {
-//     return _updateUsdRate(state, action);
-//   } else if (action is UpdateTAVTransaction) {
-//     return _updateTavTransactions(state, action);
-//   } else if (action is UpdatePublicAddress) {
-//     return _setPublicAddress(state, action);
-//   } else if (action is UpdateTavBalance) {
-//     return _updateTavBalance(state, action);
-//   } else if (action is UpdateTcpBalance) {
-//     return _updateTcpBalance(state, action);
-//   } else if (action is UpdateNotifications) {
-//     return _updateNotifications(state, action);
-//   } /* else if (action is UpdateReferCode) {
-//     return _updateRefCode(state, action);
-//   }  */
-//   else if (action is UpdateTCPTransaction) {
-//     return _updateTcpTransactions(state, action);
-//   } else if (action is UpdateTavWallet) {
-//     return _updateTavWallet(state, action);
-//   } else if (action is UpdateCouponList) {
-//     return _updateCouponList(state, action);
-//   }
+AppState appReducer(AppState state, action) {
+  if (action is UpdateCoinList) {
+    return _updateCoinList(state, action);
+  }
 
-//   return state;
-// }
+  return state;
+}
 
-// AppState _updateCouponList(AppState status, UpdateCouponList action) {
-//   return status.copyWith(
-//     couponsList: action.couponModelList,
-//   );
-// }
-
-// AppState _updateTavWallet(AppState status, UpdateTavWallet action) {
-//   return status.copyWith(
-//     tavWallet: action.tavWallet,
-//   );
-// }
-
-// AppState _updateProfile(AppState state, UpdateProfile action) {
-//   return state.copyWith(
-//     profile: action.profile,
-//   );
-// }
-
-// AppState _updateUsdRate(AppState state, UpdateUsdRate updateUsdRate) {
-//   return state.copyWith(
-//     usdRate: updateUsdRate.rate,
-//   );
-// }
-
-// AppState _updateTavTransactions(
-//     AppState state, UpdateTAVTransaction updateTAVTransaction) {
-//   return state.copyWith(
-//     tavTransactions: updateTAVTransaction.transactions,
-//   );
-// }
-
-// AppState _updateTcpTransactions(
-//     AppState state, UpdateTCPTransaction updateTcpTransaction) {
-//   return state.copyWith(
-//     tcpTransactions: updateTcpTransaction.transactions,
-//   );
-// }
-
-// AppState _setPublicAddress(
-//     AppState state, UpdatePublicAddress setPublicAddress) {
-//   MySharedPreference().saveMagicAddress(setPublicAddress.address);
-//   return state.copyWith(
-//     publicAddress: setPublicAddress.address,
-//   );
-// }
-
-// AppState _updateTavBalance(AppState state, UpdateTavBalance updateTavBalance) {
-//   return state.copyWith(
-//     tavBalance: updateTavBalance.balance,
-//   );
-// }
-
-// AppState _updateTcpBalance(AppState state, UpdateTcpBalance updateTcpBalance) {
-//   return state.copyWith(
-//     tcpBalance: updateTcpBalance.etherAmount,
-//   );
-// }
-
-// AppState _updateNotifications(
-//     AppState state, UpdateNotifications updateNotifications) {
-//   return state.copyWith(
-//     notificationList: updateNotifications.notificationList,
-//   );
-// }
-
-// /* AppState _updateRefCode(AppState state, UpdateReferCode updateReferCode) {
-//   return state.copyWith(
-//     refCode: updateReferCode.referCode,
-//   );
-// } */
+AppState _updateCoinList(AppState status, UpdateCoinList action) {
+  return status.copyWith(
+    coinList: action.coinList,
+  );
+}
